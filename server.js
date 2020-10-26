fbeb7cca78953dec685e686ca79564529a7e77eb
benyamynbrkyc <benjamin.brkic@gmail.com>
2020-10-26 23:35:54 +0100

    staging

--- server.js
+++ server.js
@@ -11,4 +11,4 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Server is listening on Port: ${PORT}`));
+app.listen(PORT, () => console.log(`Server is listening on localhost:${PORT}`));
