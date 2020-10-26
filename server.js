881e284d7b4c30c88610895d60d605c359ef004f
benyamynbrkyc <benjamin.brkic@gmail.com>
2020-10-26 23:48:20 +0100

    staging

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ app.use(express.static(__dirname));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, '/about-us.html'));
+});
+app.get('/menu', (req, res) => {
+  res.sendFile(path.join(__dirname, '/menu.pdf'));
+});
+app.get('/chefs', (req, res) => {
+  res.sendFile(path.join(__dirname, '/chefs.html'));
+});
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, '/contact.html'));
+});
 
 const PORT = process.env.PORT || 3000;
 
